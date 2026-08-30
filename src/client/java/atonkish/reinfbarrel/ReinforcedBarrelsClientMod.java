b28e9cb273c9e7d2217f2226bb5898be97b38edb
NightHawk35449 <Btaylor35449@gmail.com>
2026-08-30 19:29:32 -0400

    Add client source

---
+++ src/client/java/atonkish/reinfbarrel/ReinforcedBarrelsClientMod.java
@@ -0,0 +1,27 @@
+package atonkish.reinfbarrel;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import atonkish.reinfbarrel.util.ReinforcingMaterialSettings;
+import atonkish.reinfcore.api.ReinforcedCoreClientModInitializer;
+import atonkish.reinfcore.api.ReinforcedCoreClientRegistry;
+import atonkish.reinfcore.util.ReinforcingMaterial;
+
+@Environment(EnvType.CLIENT)
+public class ReinforcedBarrelsClientMod implements ReinforcedCoreClientModInitializer {
+  @Override
+  public void onInitializeReinforcedCoreClient() {
+    // init Reinforced Core
+    initializeReinforcedCoreClient();
+  }
+
+  private static void initializeReinforcedCoreClient() {
+    for (ReinforcingMaterialSettings materialSettings : ReinforcingMaterialSettings.values()) {
+      ReinforcingMaterial material = materialSettings.getMaterial();
+
+      // Reinforced Storage Screen
+      ReinforcedCoreClientRegistry.registerMaterialSingleBlockScreen(material);
+    }
+  }
+}
